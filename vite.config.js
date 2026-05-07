a11aeb641a99610f7abf2f63a397b446cf9c0ef2
aescms26-cmyk <aescms26@gmail.com>
2026-05-07 22:45:37 +0530

    latest

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/capstone-react/"
+  // Vercel hosts at site root by default
+  base: "/"
 })
